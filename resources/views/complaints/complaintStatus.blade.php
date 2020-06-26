78da3e9d4a9cbbcf3b4d91c0344872d1371b4678
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-06-27 01:23:44 +0300

    some styles added

--- resources/views/complaints/complaintStatus.blade.php
+++ resources/views/complaints/complaintStatus.blade.php
@@ -27,7 +27,7 @@
             </div>
             <!-- /.box-header -->
             <div class="box-body table-responsive">
-              <table id="example1" class="table table-bordered table-hover ">
+              <table id="example1" class="table table-bordered table-hover display nowrap" style="width:100%">
                 <thead>
                 <tr>
                     <th>Customer Name</th>
