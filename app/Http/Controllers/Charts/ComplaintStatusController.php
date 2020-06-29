e6b4499ce635a30f267bc049b5ae1d6c17624f8e
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-06-29 20:07:57 +0300

    words modified

--- app/Http/Controllers/Charts/ComplaintStatusController.php
+++ app/Http/Controllers/Charts/ComplaintStatusController.php
@@ -24,21 +24,21 @@ class ComplaintStatusController extends Controller
         return $month_array;
     }    
 
-    public function getMonthlyComplaintCount(){
-        $monthly_complaint_count = Complaint::whereMonth('created_at', date('m'))->get()->count('status');
+    public function getMonthlyComplaintCount($status_name){
+        $monthly_complaint_count = Complaint::whereMonth('created_at', date('m'))->where('status', $status_name)->get()->count('status');
         return($monthly_complaint_count);
     }
 
     public function getMonthlyComplaintStatus(){
         $monthly_complaint_count_array = array();
-         $month_array = $this->getAllMonths();
-         $month_name_array = array();
+         $status_array = $this->getAllMonths();
+         $status_name_array = array();
 
-         if(! empty($month_array)){
-             foreach($month_array as $month_name){
-                 $monthly_complaints_count = $this->getMonthlyComplaintCount();
+         if(! empty($status_array)){
+             foreach($status_array as $status_name){
+                 $monthly_complaints_count = $this->getMonthlyComplaintCount($status_name);
                  array_push($monthly_complaint_count_array, $monthly_complaints_count);
-                 array_push($month_name_array, $month_name);
+                 array_push($status_name_array, $status_name);
              }
               
          }
@@ -46,7 +46,7 @@ class ComplaintStatusController extends Controller
          $max = round(( $max_no + 10/2 ) / 10 ) * 10;
 
          $monthly_complaint_data_array = array(
-            'status' => $month_name_array,
+            'status' => $status_name_array,
             'complaint_count_data' => $monthly_complaint_count_array,
             'max' => $max
          );
