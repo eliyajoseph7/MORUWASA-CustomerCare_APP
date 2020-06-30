2cff4f70f5b44fea852dc841b242f72db9585134
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-07-01 00:07:04 +0300

    fixed typo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,7 +31,7 @@
           
           <div class="box box-danger">
             <div class="box-header with-border">
-              <h3 class="box-title">Complaints Stauses</h3>
+              <h3 class="box-title">Complaints Statuses</h3>
 
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
