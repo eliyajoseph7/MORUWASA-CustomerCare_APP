b4f330f2f8f0aacc3b43111f9b56d7f0f1643d0e
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-07-07 22:34:04 +0300

    api route added

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('/technician', 'Api\v1\TechnicianController');
+Route::apiResource('/customer', 'Api\v1\CustomerController');
